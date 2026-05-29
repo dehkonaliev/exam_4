6c4fdbb6f846e0e6617f152ab22b10ba73d3a0ec
Maqsudbek <dehkanaliev@gmail.com>
2026-05-29 22:11:49 +0500

    The project has been done!

--- home/views.py
+++ home/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Category, Product
 from .forms import NewProductForm
+from django.db.models import Q
 
 
 def home(request):
     if request.method == 'POST':
         search_query = request.POST.get('search', '')
-        data = Product.objects.filter(name__icontains=search_query)
+        data = Product.objects.filter(Q(name__icontains=search_query) | Q(desc__icontains=search_query))
         print(data)
     else:
         data = Product.objects.order_by('-created_at')[:10]
     return render(request, 'index.html', {'latest_10':data})
 
 def category(request):
-    products = Product.objects.all()[:20]
+    products = Product.objects.all().order_by('-created_at')[:20]
     categories = Category.objects.all()
     if request.method == "POST" and not request.POST.get('category') == 'all':
         category = request.POST.get('category')
-        products = Product.objects.filter(category=category)
+        products = Product.objects.filter(category=category).order_by('-created_at')
         return render(request, 'category.html', {'products':products, 'categories':categories, 'selected_category':category})
     return render(request, 'category.html', {'products':products, 'categories':categories})
 
