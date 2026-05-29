0e69086e59026c222cc1eeedd1f9741eaf78a90e
Maqsudbek <dehkanaliev@gmail.com>
2026-05-29 11:17:43 +0500

    I created models and migrated all

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=500, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    desc = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='media/products', default='default-product.jpg')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
